05d1b899e5e9618234fa602e9adcda9ccafac75a
Vladyslav Yevdokymenko <evdokimenko@hotmail.com>
2026-08-12 18:53:36 +0300

    initial setup

---
+++ src/js/render-functions.js
@@ -0,0 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+export const createGallery = (images) => {
+
+};
+
+export const clearGallery = () => {
+
+};
+
+export const showLoader = () => {
+
+};
+
+export const hideLoader = () => {
+
+};
+
+const options = {};
+const lightbox = new SimpleLightbox('.gallery a', options);
